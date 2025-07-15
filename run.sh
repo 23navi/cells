2252aac39bd4818fa0db1663ca6b65268b880896
23navi <navisureka23@gmail.com>
2025-07-15 22:30:39 +0530

    update deployement logic

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 docker-compose down
 # export REDIS_ENDPOINT= (make sure the env is set manually on the remote server)
-docker-compose up -d --build
+docker-compose up -d --remove-orphans
 
 # chmod -x run.sh 
 # ./run.sh

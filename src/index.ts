ddb4b492590e60fe33b9c7e4c261930cbb886ff7
23navi <navisureka23@gmail.com>
2025-07-15 23:25:40 +0530

    body size limit

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import express from 'express';
+import 'express-async-errors';
+import express, { NextFunction, Request, Response } from 'express';
 import path from 'path';
 import { createCellsRouter } from './routes/cells';
 import { connectDB } from './mongo';
@@ -11,8 +12,30 @@ export const serve = (
 
   const app = express();
 
-  app.use(express.json({ limit: '50mb' }));
+  app.use(express.json({ limit: '1mb' }));
   app.use(express.urlencoded({ extended: true }));
+
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    if (req.method === 'POST' || req.method === 'PUT') {
+      let bodySize = 0;
+      try {
+        bodySize = JSON.stringify(req.body)?.length;
+      } catch { }
+
+      const bodySizeMB = bodySize / (1024 * 1024);
+      console.log(`Request Body Size: ${bodySizeMB} MB`);
+
+      // if (bodySizeMB > 16) {
+      //   return res.status(413).json({
+      //     error: "Payload too large",
+      //     message: "The request body exceeds the 16MB limit.",
+      //     receivedSizeMB: bodySizeMB.toFixed(2)
+      //   });
+      // }
+    }
+    next();
+  });
 
   app.use(createCellsRouter());
 
@@ -20,10 +43,15 @@ export const serve = (
   const clientBuildPath = path.join(__dirname, '..', 'client', 'build');
   app.use(express.static(clientBuildPath));
 
+
   app.get('*', (req, res) => {
     res.sendFile(path.join(clientBuildPath, 'index.html'));
   });
 
+  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+    console.error('Global error handler:', err);
+    res.status(500).send({ error: err.message || 'Unknown error' });
+  })
 
   return new Promise<void>((resolve, reject) => {
     app.listen(port, resolve).on('error', reject);
--- src/mongo.ts
+++ src/mongo.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://mongo:27017/local-api', {
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/local-api', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     } as any);

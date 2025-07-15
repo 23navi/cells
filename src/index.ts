e958b146779873766a36ed3173e048a126a86feb
23navi <navisureka23@gmail.com>
2025-07-15 22:33:54 +0530

    update express body limit

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,9 @@ export const serve = (
 
   const app = express();
 
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ extended: true }));
+
   app.use(createCellsRouter());
 
   // Use path.join and __dirname for clarity
